bfae86ed1b59c56bd8ae211b428a10f9a153ad17
Leandro Toledo <lgtoledo@outlook.com>
2022-06-01 01:23:50 -0300

    Creation of entities

---
+++ ChallengeDotnetAPI/Models/Genre.cs
@@ -0,0 +1,10 @@
+﻿namespace ChallengeDotnetAPI.Models
+{
+    public class Genre
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public byte[] Image { get; set; }
+        public virtual ICollection<Movie> Movies { get; set; }
+    }
+}
---
+++ ChallengeDotnetAPI/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace ChallengeDotnetAPI.Models
+{
+    public class Movie
+    {
+        public long ID { get; set; }
+        public byte[] Image { get; set; }
+        public string Title { get; set; }
+        public int CreationDate { get; set; }
+        public decimal Score { get; set; }
+        public virtual ICollection<Character> Characters { get; set; }
+    }
+}
