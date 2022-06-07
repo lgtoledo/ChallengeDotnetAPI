f92c29a646b741dec90840f8c28b2892b26297da
Leandro Toledo <lgtoledo@outlook.com>
2022-06-06 23:53:55 -0300

    Movie controller, not implemented yet

---
+++ ChallengeDotnetAPI/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChallengeDotnetAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]    
+    public class MovieController : ControllerBase
+    {
+
+
+
+        // GET all action
+
+        
+        // GET by  title, filter by genre, and order by date
+        
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
