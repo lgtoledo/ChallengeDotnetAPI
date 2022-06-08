d96907f07d8e8fa22e3a840e9c314cb15aa0fef5
Leandro Toledo <lgtoledo@outlook.com>
2022-06-08 19:04:20 -0300

    ICharacters improved and repository CRUD finished

--- ChallengeDotnetAPI/Interface/ICharacters.cs
+++ ChallengeDotnetAPI/Interface/ICharacters.cs
@@ -7,8 +7,8 @@ namespace ChallengeDotnetAPI.Interface
         public Task<List<Character>> GetAllAsync();
         public Task<Character> GetByIdAsync(long id);
         public Task<List<Character>> SearchAsync(string? name, int? age, decimal? weight, long? movieId);
-        public Task<Character> CreateAsync(Character character);
-        public Task<Character> UpdateAsync(Character character);
+        public Task CreateAsync(Character character);
+        public Task UpdateAsync(Character character);
         public Task DeleteAsync(long id);
         public Task<bool> ExistsAsync(long id);
     }
