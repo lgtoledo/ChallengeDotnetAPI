095e00fd3769904623d24f5e21755ac9c1cff40f
Leandro Toledo <lgtoledo@outlook.com>
2022-06-08 19:14:29 -0300

    Extension for ModelState to return error messages of validation

---
+++ ChallengeDotnetAPI/Extensions/ModelStateExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Linq;
+
+namespace ChallengeDotnetAPI.Extensions
+{
+    public static class ModelStateExtensions
+    {
+        public static List<String> GetErrorMessages(this ModelStateDictionary modelState)
+        {
+            // return friendly error messages for ModelState validation errors
+            return modelState.Values
+                .SelectMany(x => x.Errors)
+                .Select(x => x.ErrorMessage)
+                .ToList();
+        }
+    }
+}
